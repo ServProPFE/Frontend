0f7e56cb2b7a2a09e99138eb0116a90ea0a35e45
hechmi10 <hechmi.jaidane@gmail.com>
2026-05-07 15:46:23 +0100

    Fix crash

--- src/pages/ProviderPortfolio.jsx
+++ src/pages/ProviderPortfolio.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import apiService from '../services/apiService';
